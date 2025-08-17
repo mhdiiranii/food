f0d40b6333f4453c0b522a085e12e16c2dae9213
MAHDI <mhdiiranii@gmail.com>
2025-08-18 00:01:43 +0330

    cta

--- src/component/home-layout/cta.jsx
+++ src/component/home-layout/cta.jsx
@@ -1,7 +1,16 @@
+import Button from '../button'
+
 const Cta = () => {
     return ( 
         <div className="cta">
-            
+            <div className="cta-box">
+                <h3 className="cta-box-h3">
+                    Are you ready to order with the best deals?
+                </h3>
+                <Button className = {'button-cta'}>
+                    Proceed to order {'>'}
+                </Button>
+            </div>
         </div>
      );
 }
