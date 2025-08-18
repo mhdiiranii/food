5917dec6247ff8dccd571e2be98225585f83dc33
MAHDI <mhdiiranii@gmail.com>
2025-08-18 15:55:19 +0330

    fix bug

--- src/component/render-image.jsx
+++ src/component/render-image.jsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from "react";
 
-// const Images = lazy(() => import("./image"));
-const Images = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import("./image")), 10000); // ۳ ثانیه تاخیر
-  });
-});
+const Images = lazy(() => import("./image"));
+// const Images = lazy(() => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(import("./image")), 10000); // ۳ ثانیه تاخیر
+//   });
+// });
 
 const Loader = ({className})=>{
     return(
